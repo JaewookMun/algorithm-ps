69e5ebfd37ef08556a362f9f6d9844b02a8de6f5
Jaewook Mun <mun1103.dev@gmail.com>
2023-09-09 23:52:16 +0900

    [Silver IV] Title: 문자열, Time: 204 ms, Memory: 17624 KB -BaekjoonHub

---
+++ 백준/Silver/1120. 문자열/문자열.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String a = sc.next();
+        String b = sc.next();
+
+        int result = a.length();
+
+        for (int i = 0; i < b.length() - a.length() + 1; i++) {
+            int tmp = 0;
+            for (int j = 0; j < a.length(); j++) {
+                if (a.charAt(j) != b.charAt(j + i)) {
+                    tmp++;
+                }
+            }
+            if (result > tmp) {
+                result = tmp;
+            }
+        }
+        System.out.println(result);
+        sc.close();
+    }
+}
